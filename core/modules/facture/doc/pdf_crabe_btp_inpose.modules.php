5fff3a1cd20c8c037d414eedeeb39214c6514940
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-31 01:15:51 +0100

    Adjust table height for PDF generation

--- core/modules/facture/doc/pdf_crabe_btp_inpose.modules.php
+++ core/modules/facture/doc/pdf_crabe_btp_inpose.modules.php
@@ -399,10 +399,10 @@ class pdf_crabe_btp_inpose extends ModelePDFFactures
 
 				$tab_top = 90;
 				$tab_top_newpage = (empty($conf->global->MAIN_PDF_DONOTREPEAT_HEAD)?42:10);
-				$tab_height = 155;
+				$tab_height = 130;
 				$tab_height_newpage = 150;
 
-				$this->_tableauBtp($pdf, $tab_top, $this->page_hauteur - 100 - $heightforfreetext - $heightforfooter, 0, $outputlangs, 0, 0, $object->multicurrency_code);
+				$this->_tableauBtp($pdf, $tab_top, $this->page_hauteur - 80 - $heightforfreetext - $heightforfooter, 0, $outputlangs, 0, 0, $object->multicurrency_code);
 				$bottomlasttab=$this->page_hauteur - $heightforinfotot - $heightforfreetext - $heightforfooter + 1;
 
 				$this->_pagefoot($pdf,$object,$outputlangs,1);
