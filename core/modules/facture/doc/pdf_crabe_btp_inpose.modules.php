810419b3ce5f02d34b78e288741fbef6c623e959
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-31 20:36:23 +0100

    Update pdf_crabe_btp_inpose.modules.php

--- core/modules/facture/doc/pdf_crabe_btp_inpose.modules.php
+++ core/modules/facture/doc/pdf_crabe_btp_inpose.modules.php
@@ -1817,13 +1817,12 @@ class pdf_crabe_btp_inpose extends ModelePDFFactures
 						$prev_total_tva += (float) $line->total_tva;
 						$prev_total_ttc += (float) $line->total_ttc;
 					}
+					var_dump($prev_total_ht );//= 0;
+					var_dump($prev_total_tva);// = 0;
+					var_dump($prev_total_ttc);// = 0;
 				}
 			}
 		}
-
-		var_dump($prev_total_ht );//= 0;
-		var_dump($prev_total_tva);// = 0;
-		var_dump($prev_total_ttc);// = 0;
 
 		$current_total_ht = 0;
 		$current_total_tva = 0;
@@ -1842,9 +1841,9 @@ class pdf_crabe_btp_inpose extends ModelePDFFactures
 				$current_total_ttc += (float) $line->total_ttc;
 			}
 		}
-		$cumulative_total_ht = $prev_total_ht - $current_total_ht;
-		$cumulative_total_tva = $prev_total_tva - $current_total_tva;
-		$cumulative_total_ttc = $prev_total_ttc - $current_total_ttc;
+		$cumulative_total_ht = $prev_total_ht ;//- $current_total_ht;
+		$cumulative_total_tva = $prev_total_tva ;//- $current_total_tva;
+		$cumulative_total_ttc = $prev_total_ttc ;//- $current_total_ttc;
 
 		$retained_warranty_rate = (! empty($object->retained_warranty) ? $object->retained_warranty : 0);
 		$retenue_de_garantie = $total_ttc * $retained_warranty_rate / 100;
